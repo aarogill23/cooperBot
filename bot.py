7339cf8af7782d5ff027d4035c1909ddb4ddce84
Aaron Gill <angill23@live.com>
2024-01-29 00:04:35 -0600

    Create bot.py
    Created bot. Three simple functions.

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv("token.env")
+TOKEN = os.getenv('TOKEN')
+
+class myClient(discord.Client):
+
+    async def on_ready(self):
+        print(f'We have logged in as {self.user}')
+    
+    async def on_message(self, message):
+        print(f"{message.channel}: {message.author}: {message.content}")
+
+        if message.author == client.user:
+            return
+        
+        if message.content.startswith('!ping'):
+            await message.channel.send('pong!')
+        
+    async def on_message_delete(self, message):
+        print(f"Deleted message from {message.author}: {message.content}")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+
+client = myClient(intents=intents)
+client.run(TOKEN)
+
+
