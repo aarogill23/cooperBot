3de56b0bf79f87af72f5d6e7aeb1a2cbf0882360
Aaron Gill <angill23@live.com>
2024-01-30 15:30:07 -0600

    Added events for when the user passes in an incorrect command, deletes a message and edits a message.

--- bot.py
+++ bot.py
@@ -33,6 +33,25 @@ async def on_ready():
 async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
     await bot.process_commands(message)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command not found use !help for a list of commands")
+
+@bot.event
+async def on_message_delete(message):
+    print(f"In {message.channel}: {message.author}: deleted a message that said {message.content}")
+
+@bot.event
+async def on_message_edit(before, after):
+    print(f"In {before.channel}: {before.author}: edited a message from {before.content} to {after.content}")
+
+@bot.event
+async def on_user_join(ctx, member):
+    await ctx.send(f"Welcome {member.name} to the server!")
+    print(f"{member.name} joined the server")
+
 bot.run(TOKEN)
 
 
